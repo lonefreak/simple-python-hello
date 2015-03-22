94ee68bf690881ebc481e20dd214419dd6ca0e6f
Lonefreak <fabricio.leotti@gmail.com>
2015-03-22 20:24:02 -0300

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,25 @@
+import os
+from setuptools import setup
+
+# Utility function to read the README file.
+# Used for the long_description.  It's nice, because now 1) we have a top level
+# README file and 2) it's easier to type in the README file than to put a raw
+# string in below ...
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+    name = "hello",
+    version = "0.1.0",
+    author = "Fabricio Leotti",
+    author_email = "fabricio.leotti@gmail.com",
+    description = ("A demonstration of a simple hello-world python application using Flask"),
+    license = "BSD",
+    keywords = "example flask hello",
+    url = "http://www.hellopython.com",
+    long_description=read('README'),
+    classifiers=[
+        "Development Status :: Beta",
+        "Topic :: Demonstration",
+    ],
+)
